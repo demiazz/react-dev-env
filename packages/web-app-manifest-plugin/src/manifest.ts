11f0115fff9bb2dbae0fec36de065fd23de20586
Alexey Plutalov <demiazz.py@gmail.com>
2020-01-22 00:20:09 +0300

    web-app-manifest-plugin: Add `Image`, `FingerPrint`, `RelatedApplication`, `ServiceWorker` and `Manifest` types

---
+++ packages/web-app-manifest-plugin/src/manifest.ts
@@ -0,0 +1,50 @@
+import { Dir, Display, Orientation, Platform } from "./enums";
+
+export interface Image {
+  src: string;
+  sizes?: string;
+  type?: string;
+  purpose?: string;
+  platform?: string;
+}
+
+export interface FingerPrint {
+  type: string;
+  value: string;
+}
+
+export interface RelatedApplication {
+  platform: Platform;
+  url?: string;
+  id?: string;
+  min_version?: string;
+  fingerprints?: FingerPrint[];
+}
+
+export interface ServiceWorker {
+  src?: string;
+  scope?: string;
+  type?: string;
+  update_via_cache?: string;
+}
+
+export interface Manifest {
+  background_color?: string;
+  categories?: string[];
+  description?: string;
+  dir?: Dir;
+  display?: Display;
+  iarc_rating_id?: string;
+  icons: Image[];
+  lang?: string;
+  name: string;
+  orientation?: Orientation;
+  prefer_related_applications?: boolean;
+  related_applications?: RelatedApplication[];
+  scope?: string;
+  screenshots?: Image[];
+  serviceworker?: ServiceWorker;
+  short_name?: string;
+  start_url?: string;
+  theme_color?: string;
+}
