9467ffac231c2de0afd4548869e08196eb4bfaae
Alexey Plutalov <demiazz.py@gmail.com>
2020-01-21 19:08:33 +0300

    utils: Add `JSONSource` class

---
+++ packages/utils/src/JSONSource.ts
@@ -0,0 +1,19 @@
+import { RawSource } from "webpack-sources";
+
+type JSONPrimitive = string | number | boolean | null;
+
+type JSONObject = {
+  [property: string]: JSONValue;
+};
+
+type JSONArray = JSONValue[];
+
+type JSONValue = JSONPrimitive | JSONObject | JSONArray;
+
+export class JSONSource extends RawSource {
+  public constructor(value: JSONValue, beautify: false) {
+    const source = JSON.stringify(value, null, beautify ? 2 : 0);
+
+    super(source);
+  }
+}
