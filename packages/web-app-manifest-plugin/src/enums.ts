022f6bd9457696f6a4e2b58a2bc60b9f4edb819f
Alexey Plutalov <demiazz.py@gmail.com>
2020-01-22 12:20:03 +0300

    web-app-manifest-plugin: Remove validation functions from enums

--- packages/web-app-manifest-plugin/src/enums.ts
+++ packages/web-app-manifest-plugin/src/enums.ts
@@ -1,21 +1,6 @@
 export type Dir = "auto" | "ltr" | "rtl";
 
-export const possibleDirValues: Dir[] = ["auto", "ltr", "rtl"];
-
-export const isDir = (value: any): value is Dir =>
-  possibleDirValues.includes(value as any);
-
 export type Display = "fullscreen" | "standalone" | "minimal-ui" | "browser";
-
-export const possibleDisplayValues: Display[] = [
-  "fullscreen",
-  "standalone",
-  "minimal-ui",
-  "browser"
-];
-
-export const isDisplay = (value: any): value is Display =>
-  possibleDisplayValues.includes(value);
 
 export type Orientation =
   | "any"
@@ -27,30 +12,6 @@ export type Orientation =
   | "landscape-primary"
   | "landscape-secondary";
 
-export const possibleOrientationValues: Orientation[] = [
-  "any",
-  "natural",
-  "landscape",
-  "portrait",
-  "portrait-primary",
-  "portrait-secondary",
-  "landscape-primary",
-  "landscape-secondary"
-];
+export type Platform = "play" | "itunes" | "windows";
 
-export const isOrientation = (value: any): value is Orientation =>
-  possibleOrientationValues.includes(value);
-
-export type Platform = "chrome_web_store" | "play" | "itunes" | "windows";
-
-const platformRegExp = /^(chrome_web_store|play|itunes|windows)$/;
-
-export const isPlatform = (value: string): value is Platform =>
-  platformRegExp.test(value);
-
-export type ImagePurpose = "any" | "badge" | "maskable";
-
-const imagePurposeRegExp = /^(any|badge|maskable)$/;
-
-export const isImagePurpose = (value: string): value is ImagePurpose =>
-  imagePurposeRegExp.test(value);
+export type Purpose = "any" | "badge" | "maskable";
