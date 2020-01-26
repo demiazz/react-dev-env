f8e9fd16f7c312644b2442ac631783366703b328
Alexey Plutalov <demiazz.py@gmail.com>
2020-01-26 13:58:02 +0300

    web-app-manifest-plugin: Adds options validation and assets and manifest generation

--- packages/web-app-manifest-plugin/src/index.ts
+++ packages/web-app-manifest-plugin/src/index.ts
@@ -1,33 +1,50 @@
-import validateManifest from "schema-utils";
+import validateOptions from "schema-utils";
 import { Compiler, Plugin } from "webpack";
 
-import manifestSchema from "./schemas/manifest.json";
-
-import { Manifest } from "./types";
+import { prepareAssetsAndManifest } from "./manifest";
+import manifestOptionsSchema from "./schemas/manifestOptions.json";
+import optionsSchema from "./schemas/options.json";
+import { ManifestOptions } from "./types";
 
 interface Options {
   beautify?: boolean;
-  fileName?: string;
 }
 
 export class WebAppManifestPlugin implements Plugin {
-  public readonly manifest: Manifest;
+  public readonly manifestOptions: ManifestOptions;
   public readonly beautify: boolean;
-  public readonly fileName: string;
 
-  public constructor(
-    manifest: Manifest,
-    { beautify = false, fileName = "manifest.json" }: Options = {}
-  ) {
-    validateManifest(manifestSchema, manifest, {
+  public constructor(manifestOptions: ManifestOptions, options: Options = {}) {
+    validateOptions(manifestOptionsSchema, manifestOptions, {
       name: "WebAppManifestPlugin",
       baseDataPath: "manifest"
     });
+    validateOptions(optionsSchema, options, {
+      name: "WebAppManifestPlugin",
+      baseDataPath: "options"
+    });
 
-    this.manifest = manifest;
-    this.beautify = beautify;
-    this.fileName = fileName;
+    this.manifestOptions = manifestOptions;
+    this.beautify = options.beautify ?? false;
   }
 
-  public apply(_compiler: Compiler): void {}
+  public apply(compiler: Compiler): void {
+    compiler.hooks.emit.tapPromise(
+      "@react-dev-env/web-app-manifest-plugin",
+      async compilation => {
+        const { assets } = await prepareAssetsAndManifest(
+          this.manifestOptions,
+          {
+            beautify: this.beautify,
+            context: compiler.options.context ?? process.cwd(),
+            publicPath: compiler.options.output?.publicPath ?? ""
+          }
+        );
+
+        for (const { filePath, source } of assets) {
+          compilation.assets[filePath] = source;
+        }
+      }
+    );
+  }
 }
