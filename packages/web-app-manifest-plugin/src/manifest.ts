0b4b76451d49ae7d5a5e67d7b5e95b78a9e4cefa
Alexey Plutalov <demiazz.py@gmail.com>
2020-01-26 13:57:38 +0300

    web-app-manifest-plugin: Rewrites manifest and assets generation

--- packages/web-app-manifest-plugin/src/manifest.ts
+++ packages/web-app-manifest-plugin/src/manifest.ts
@@ -1,35 +1,36 @@
+import { JSONSource, JSONValue } from "@react-dev-env/utils";
+
+import { readAssetsAndResources } from "./images";
 import {
-  WebAppManifestRelatedApplication,
-  RelatedApplication,
+  Asset,
+  ExtendedImageResource,
+  ExternalApplicationResource,
+  ImageResource,
   Manifest,
+  ManifestOptions,
+  RelatedApplication,
   WebAppManifest
 } from "./types";
-import { normalizeImage, normalizeImages } from "./images";
 
-export function normalizeManifest(manifest: Manifest, context: string) {
-  normalizeImage(manifest.favIcon, context);
-
-  if (manifest.appIcons) {
-    normalizeImages(manifest.appIcons, context);
-  }
-
-  if (manifest.screenshots) {
-    normalizeImages(manifest.screenshots, context);
-  }
+function serializeExtendedImageResource({
+  appleTouchIcon,
+  ...resource
+}: ExtendedImageResource): ImageResource {
+  return resource;
 }
 
 function serializeRelatedApplication({
   minVersion,
-  ...relatedApplication
-}: RelatedApplication): WebAppManifestRelatedApplication {
+  ...application
+}: RelatedApplication): ExternalApplicationResource {
   return {
-    min_version: minVersion,
+    ...application,
 
-    ...relatedApplication
+    min_version: minVersion
   };
 }
 
-export function serializeManifest({
+function serializeManifest({
   appIcons,
   backgroundColor,
   favIcon,
@@ -40,21 +41,103 @@ export function serializeManifest({
   shortName,
   startUrl,
   themeColor,
-  ...manifest
+  ...manifestProperties
 }: Manifest): WebAppManifest {
-  return {
+  const icons = [serializeExtendedImageResource(favIcon)];
+
+  if (appIcons && appIcons.length > 0) {
+    icons.push(...appIcons.map(serializeExtendedImageResource));
+  }
+
+  const webAppManifest: WebAppManifest = {
+    ...manifestProperties,
+
     background_color: backgroundColor,
     iarc_rating_id: iarcRatingId,
     prefer_related_applications: preferRelatedApplication,
-    related_applications: relatedApplications
-      ? relatedApplications.map(serializeRelatedApplication)
-      : undefined,
     short_name: shortName,
     start_url: startUrl,
     theme_color: themeColor,
 
-    icons: [],
-
-    ...manifest
+    icons
   };
+
+  if (relatedApplications && relatedApplications.length > 0) {
+    webAppManifest.related_applications = relatedApplications.map(
+      serializeRelatedApplication
+    );
+  }
+
+  if (screenshots && screenshots.length > 0) {
+    webAppManifest.screenshots = screenshots;
+  }
+
+  return webAppManifest;
+}
+
+export async function prepareAssetsAndManifest(
+  { appIcons, favIcon, screenshots, ...manifestProperties }: ManifestOptions,
+  options: {
+    beautify: boolean;
+    context: string;
+    publicPath: string;
+  }
+): Promise<{ assets: Asset[]; manifest: Manifest }> {
+  const assets: Asset[] = [];
+
+  const {
+    assets: [favIconAsset],
+    resources: [favIconResource]
+  } = await readAssetsAndResources([{ filePath: favIcon }], {
+    ...options,
+    fileName: "favicon"
+  });
+
+  assets.push(favIconAsset);
+
+  const manifest: Manifest = {
+    ...manifestProperties,
+
+    favIcon: favIconResource
+  };
+
+  if (appIcons && appIcons.length > 0) {
+    const {
+      assets: appIconsAssets,
+      resources: appIconsResources
+    } = await readAssetsAndResources(appIcons, {
+      ...options,
+      fileName: "web-application/app-icons/app-icon"
+    });
+
+    assets.push(...appIconsAssets);
+    manifest.appIcons = appIconsResources;
+  } else {
+    manifest.appIcons = undefined;
+  }
+
+  if (screenshots && screenshots.length > 0) {
+    const {
+      assets: screenshotsAssets,
+      resources: screenshotsResources
+    } = await readAssetsAndResources(screenshots, {
+      ...options,
+      fileName: "web-application/screenshots/screenshot"
+    });
+
+    assets.push(...screenshotsAssets);
+    manifest.screenshots = screenshotsResources;
+  } else {
+    manifest.screenshots = undefined;
+  }
+
+  assets.push({
+    filePath: "web-application/manifest.json",
+    source: new JSONSource(
+      (serializeManifest(manifest) as unknown) as JSONValue,
+      options.beautify
+    )
+  });
+
+  return { assets, manifest };
 }
