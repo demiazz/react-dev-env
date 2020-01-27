4f5c2531330c3c02efb63ec5bb2c4587185f3ec1
Alexey Plutalov <demiazz.py@gmail.com>
2020-01-28 00:31:51 +0300

    webpack-aspects: Refactor tests for `checkBooleanEnvironmentVariable`

--- packages/webpack-aspects/__tests__/utils.test.ts
+++ packages/webpack-aspects/__tests__/utils.test.ts
@@ -5,60 +5,41 @@ describe("@react-dev-env/webpack-aspects", () => {
   describe("utils", () => {
     describe("checkBooleanEnvironmentVariable", () => {
       let environmentBackup: NodeJS.ProcessEnv;
-      let variableName: string;
 
       beforeEach(() => {
         environmentBackup = process.env;
 
         process.env = {};
-
-        variableName = faker.random
-          .word()
-          .replace(" ", "_")
-          .toUpperCase();
       });
 
       afterEach(() => {
         process.env = environmentBackup;
       });
 
-      describe("when variable is not defined", () => {
-        it("returns `false`", () => {
-          expect(checkBooleanEnvironmentVariable(variableName)).toBe(false);
+      const samples: [string | undefined, boolean][] = [
+        [undefined, false],
+        ["false", false],
+        [faker.random.uuid(), false],
+        ["1", true],
+        ["true", true]
+      ];
+
+      for (const [value, expected] of samples) {
+        const variableName = faker.random
+          .word()
+          .replace(" ", "_")
+          .toUpperCase();
+
+        describe(`when variable is equals to "${value || "undefined"}"`, () => {
+          it(`returns "${expected}"`, () => {
+            process.env[variableName] = value;
+
+            const actual = checkBooleanEnvironmentVariable(variableName);
+
+            expect(actual).toBe(expected);
+          });
         });
-      });
-
-      describe("when variable is equals to `1`", () => {
-        it("returns `true`", () => {
-          process.env[variableName] = "1";
-
-          expect(checkBooleanEnvironmentVariable(variableName)).toBe(true);
-        });
-      });
-
-      describe("when variable is equals to `true`", () => {
-        it("returns `true`", () => {
-          process.env[variableName] = "true";
-
-          expect(checkBooleanEnvironmentVariable(variableName)).toBe(true);
-        });
-      });
-
-      describe("when variable is equals to `false`", () => {
-        it("returns `false`", () => {
-          process.env[variableName] = "false";
-
-          expect(checkBooleanEnvironmentVariable(variableName)).toBe(false);
-        });
-      });
-
-      describe("when variable is equals to any value", () => {
-        it("returns `false`", () => {
-          process.env[variableName] = faker.random.uuid();
-
-          expect(checkBooleanEnvironmentVariable(variableName)).toBe(false);
-        });
-      });
+      }
     });
   });
 });
